b597758636499f06db54aafa64525ae7a109181c
Mikhail Kopkin <v1rasta163@gmail.com>
2022-10-26 23:29:08 +0400

    homework_1

--- Seminars/Homeworks/Homework_001/Task_003.py
+++ Seminars/Homeworks/Homework_001/Task_003.py
@@ -5,3 +5,15 @@
 # x=34; y=-30 -> 4
 # x=2; y=4-> 1
 # x=-34; y=-30 -> 3
+
+x, y = float(input('x = ')), float(input('y = '))
+if x > 0 and y > 0:
+    print('1')
+elif x < 0 and y > 0:
+    print('2')
+elif x < 0 and y < 0:
+    print('3')
+elif x > 0 and y <0:
+    print('4')
+else:
+    ('input error')
